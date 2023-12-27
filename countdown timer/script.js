83f92409ea06359e9617d6134662b034d7b9cf53
souchit21 <souchit21@gmail.com>
2023-12-27 10:46:36 +0530

    added countdown timer app

---
+++ countdown timer/script.js
@@ -0,0 +1,30 @@
+const DAYS = document.getElementById("days");
+const HOURS = document.getElementById("hours");
+const  MINUTES= document.getElementById("mins");
+const SECONDS = document.getElementById("seconds");
+
+
+const newYears = '1 Jan 2023';
+
+function countDown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalseconds = (newYearsDate-currentDate)/1000;
+    const days = Math.floor(totalseconds/3600/24);
+    const hours = Math.floor(totalseconds/3600 % 24);
+    const minutes = Math.floor(totalseconds /60 % 60);
+    const seconds = Math.floor(totalseconds % 60);
+    console.log(days, hours, minutes, seconds);
+    DAYS.innerHTML = days;
+    HOURS.innerHTML = edit(hours);
+    MINUTES.innerHTML = edit(minutes);
+    SECONDS.innerHTML = edit(seconds);
+}
+function edit(time){
+    if(time<10){
+        return '0'+ time;
+    }
+    return time;
+}
+countDown();
+setInterval(countDown, 1000);
